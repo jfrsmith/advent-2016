afa20ab15ff6eab460885c4e6e4634def5f78827
Jack Smith <jfrsmith@me.com>
2016-12-01 16:04:39 +0000

    day_1 complete

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -16,20 +16,20 @@ enum Facing {
 }
 
 struct DirectionPair(Direction, i32);
-struct Location(Facing, i32, i32);
+struct Path(Facing, Vec<(i32, i32)>);
 
 fn main() {
     let directions: Vec<u8> = File::open("input/input.txt").unwrap().bytes().map(|b| b.unwrap()).collect();
     let directions_str : String = String::from_utf8_lossy(&directions).split_whitespace().collect();
 
-    let Location(final_facing, final_x, final_y) : Location = directions_str.split(',').map(|d| {
+    let Path(final_facing, path) : Path = directions_str.split(',').map(|d| {
         let (dir, dist) = d.split_at(1);
         DirectionPair(match dir {
             "R" => Direction::Right,
             "L" => Direction::Left,
             _   => panic!("Invalid direction letter: {}", dir)
         }, dist.parse().unwrap())
-    }).fold(Location(Facing::North,0,0), |Location(facing, x, y), DirectionPair(dir, dist)| {
+    }).fold(Path(Facing::North, vec![(0,0)]), | Path(facing, history), DirectionPair(dir, dist)| {
         let new_facing = match dir {
             Direction::Right => match facing {
                Facing::North => Facing::East,
@@ -45,18 +45,31 @@ fn main() {
             }
         };
 
-        let (new_x, new_y) = match new_facing {
-            Facing::North => (x, y + dist),
-            Facing::South => (x, y - dist),
-            Facing::East => (x + dist, y),
-            Facing::West => (x - dist, y)
-        };
+        let (old_x, old_y) = (history.last().unwrap().0, history.last().unwrap().1);
+        let visited : Vec<(i32,i32)> = (0..dist).map(|x|{
+            match new_facing {
+            Facing::North => (old_x, old_y + (x+1)),
+            Facing::South => (old_x, old_y - (x+1)),
+            Facing::East => (old_x + (x+1), old_y),
+            Facing::West => (old_x - (x+1), old_y)
+            }
+        }).collect();
 
-        println!("Facing {:?}, X = {}, Y = {}", new_facing, new_x, new_y);
-
-        Location(new_facing, new_x, new_y)
+        Path(new_facing, history.iter().chain(visited.iter()).cloned().collect())
     });
 
+    let (final_x, final_y) = (path.last().unwrap().0, path.last().unwrap().1);
     println!("Final Location {:?}, X = {}, Y = {}", final_facing, final_x, final_y);
-    println!("Final Distance = {}", (final_x + final_y).abs());
+    println!("Final Distance = {}", (final_x.abs() + final_y.abs()).abs());
+
+    let repeats : Vec<(&(i32,i32),i32)> = path.iter().enumerate().filter_map(|x| {
+        let location : &(i32, i32) = x.1;
+        match path.split_at((x.0)+1).1.contains(&location) {
+            true => Some((location, location.0.abs() + location.1.abs())),
+            false => None        
+        }
+    }).collect();
+
+    let (first_repeat_location, first_repeat_distance) = (repeats.first().unwrap().0, repeats.first().unwrap().1);
+    println!("First repeat = ({},{}), Distance = {}", first_repeat_location.0, first_repeat_location.1, first_repeat_distance);
 }
