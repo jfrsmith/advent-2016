59c535867f366b9fd77cf448f3cd89dda724860d
Jack Smith <jfrsmith@me.com>
2016-12-01 12:16:01 +0000

    day 1 part 1 complete

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,27 +1,62 @@
 use std::io::prelude::*;
 use std::fs::File;
 
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right
+}
+
+#[derive(Debug)]
+enum Facing {
+    North,
+    South,
+    East,
+    West
+}
+
+struct DirectionPair(Direction, i32);
+struct Location(Facing, i32, i32);
+
 fn main() {
-    let mut file = File::open("input/input.txt").unwrap();
-    let mut input_str = String::new();
-    file.read_to_string(&mut input_str).unwrap();
+    let directions: Vec<u8> = File::open("input/input.txt").unwrap().bytes().map(|b| b.unwrap()).collect();
+    let directions_str : String = String::from_utf8_lossy(&directions).split_whitespace().collect();
 
-    let mut floor = 0;
-    let mut instructions = 0;
-    let mut first_basement_entry = -1;
-    for c in input_str.chars() {
-        match c {
-            '(' => floor += 1,
-            ')' => floor -= 1,
-			_ => {}
-        }
+    let Location(final_facing, final_x, final_y) : Location = directions_str.split(',').map(|d| {
+        let (dir, dist) = d.split_at(1);
+        DirectionPair(match dir {
+            "R" => Direction::Right,
+            "L" => Direction::Left,
+            _   => panic!("Invalid direction letter: {}", dir)
+        }, dist.parse().unwrap())
+    }).fold(Location(Facing::North,0,0), |Location(facing, x, y), DirectionPair(dir, dist)| {
+        let new_facing = match dir {
+            Direction::Right => match facing {
+               Facing::North => Facing::East,
+               Facing::South => Facing::West,
+               Facing::East => Facing::South,
+               Facing::West => Facing::North
+            },
+            Direction::Left => match facing {
+               Facing::North => Facing::West,
+               Facing::South => Facing::East,
+               Facing::East => Facing::North,
+               Facing::West => Facing::South
+            }
+        };
 
-        instructions += 1;
+        let (new_x, new_y) = match new_facing {
+            Facing::North => (x, y + dist),
+            Facing::South => (x, y - dist),
+            Facing::East => (x + dist, y),
+            Facing::West => (x - dist, y)
+        };
 
-        if first_basement_entry == -1 && floor == -1 {
-            first_basement_entry = instructions;
-        }
-    }
+        println!("Facing {:?}, X = {}, Y = {}", new_facing, new_x, new_y);
 
-    println!("floor = {} first_basement_entry = {}", floor, first_basement_entry);
+        Location(new_facing, new_x, new_y)
+    });
+
+    println!("Final Location {:?}, X = {}, Y = {}", final_facing, final_x, final_y);
+    println!("Final Distance = {}", (final_x + final_y).abs());
 }
