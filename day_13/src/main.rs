0ac2a7aad3bf3d86a507e60cb3ca57790d99f857
Jack Smith <jfrsmith@me.com>
2016-12-27 22:41:08 +0000

    day_13 complete

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -1,118 +1,186 @@
-use std::io::prelude::*;
-use std::fs::File;
-use std::collections::{HashMap,HashSet};
+use std::fmt;
+use std::collections::HashMap;
+use std::collections::BinaryHeap;
 
-fn main() {
-   let mut file = File::open("input/input.txt").unwrap();
-   let mut input = String::new();
-   file.read_to_string(&mut input).unwrap();
-   
-   let (happiness, guests) = get_guests(&input);
-   let happiness = calculate_happiness(guests, happiness);
-   
-   println!("{}", happiness);
+#[derive(PartialEq, Eq)]
+enum LocationType {
+    Wall,
+    Open,
 }
 
-fn calculate_happiness(guests: Vec<&str>, happy_map: HashMap<&str, HashMap<&str, i32>>) -> i32 {
-    let mut table_layouts: Vec<i32> = get_permutations(guests).into_iter().map(|p| {
-    	
-    	//println!("---------------");
-
-    	let mut happiness = 0;
-
-    	for i in 0..p.len() {
-    		let array_len: i32 = p.len() as i32;
-    		let mut left_index: i32 = i as i32 - 1;
-    		let mut right_index: i32 = i as i32 + 1;
-
-    		if left_index < 0 {
-    			left_index = array_len - 1;
-    		}
-
-    		if right_index == array_len {
-    			right_index = 0;
-    		}
-
-    	    let left_index: usize = left_index as usize;
-    	    let right_index: usize = right_index as usize;
-    	    let ref guest_map = happy_map.get(p[i]).unwrap();
-    	    let left_happy = *guest_map.get(p[left_index]).unwrap();
-    	    let right_happy = *guest_map.get(p[right_index]).unwrap();
-
-    	    //println!("{} {} {}", left_happy, p[i], right_happy);
-
-    	    happiness += left_happy + right_happy;
-    	}
-
-    	//println!("{}", happiness);
-    	//println!("---------------");
-
-        happiness
-
-    }).collect();
-
-    table_layouts.sort();
-
-    *table_layouts.last().unwrap()
-}
-
-fn get_permutations<T: Clone>(v: Vec<T>) -> Vec<Vec<T>> {
-    match v.len() {
-        0 | 1 => vec![v],
-        2 => {
-            let rev0 = v.get(1).unwrap().clone();
-            let rev1 = v.get(0).unwrap().clone();
-            vec![v, vec![rev0, rev1]]
-        },
-        _ => {
-            let mut permutations = vec![];
-            for i in 0..v.len() {
-                let mut v2 = v.to_vec();
-                v2.swap(0, i);
-                let curr = v2.get(0).unwrap().clone();
-                v2.remove(0);
-                for mut p in get_permutations(v2.to_vec()) {
-                    p.insert(0, curr.clone());
-                    permutations.push(p);
-                }
-            }
-            permutations
-        },
+impl fmt::Debug for LocationType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match self {
+            &LocationType::Wall => "#",
+            &LocationType::Open => "."
+        })
     }
 }
 
-fn get_guests(input: &String) -> (HashMap<&str, HashMap<&str, i32>>, Vec<&str>) {
-    let mut happiness: HashMap<&str, HashMap<&str, i32>> = HashMap::new();
-    let mut guests: HashSet<&str> = HashSet::new();
-    for line in input.lines() {
-        let split: Vec<&str> = line.split(|c| c == ' ' || c == '.').collect();
-        let guest = split[0];
-        let happiness_val = split[3].parse::<i32>().unwrap() * if split[2] == "gain" { 1 } else { -1 };
-        let neigbour = split[10];
-       	
-       	//println!("{}: G={}, N={}, h={}", line, guest, neigbour, happiness_val);
-        guests.insert(guest);
-        guests.insert(neigbour);
-
-        if !happiness.contains_key(guest) {
-    	    let mut neighbour_map: HashMap<&str, i32> = HashMap::new();
-    	    neighbour_map.insert("Jack", 0);
-        	neighbour_map.insert(neigbour, happiness_val);
-        	happiness.insert(guest, neighbour_map);
+impl LocationType {
+    fn construct(input: u32) -> LocationType {
+        if input % 2 == 0 {
+            LocationType::Open
         } else {
-            happiness.get_mut(guest).unwrap().insert(neigbour, happiness_val);
+            LocationType::Wall
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Node {
+    location: (usize, usize),
+    distance: usize
+}
+
+impl Eq for Node {}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Node) -> bool {
+        self.location == other.location
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Node) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Node) -> std::cmp::Ordering {
+        other.distance.cmp(&self.distance)
+    }
+}
+
+fn get_location_type((x,y): (u64,u64), designer_num: u64) -> LocationType {
+    LocationType::construct(((x*x + 3*x + 2*x*y + y + y*y) + designer_num).count_ones())
+}
+
+fn get_neighbours(node: (usize, usize), designer_num: u64) -> Vec<(usize, usize)> {
+    (0..4).filter_map(|x| {
+        let neighbour = match x {
+            0 => if node.0 > 0 {Some((node.0 - 1, node.1))} else {None},
+            1 => if node.1 > 0 {Some((node.0, node.1 - 1))} else {None},
+            2 => Some((node.0 + 1, node.1)),
+            3 => Some((node.0, node.1 + 1)),
+            _ => None
+        };
+
+        if neighbour.is_some() && get_location_type((neighbour.unwrap().0 as u64, neighbour.unwrap().1 as u64), designer_num) == LocationType::Open {
+            neighbour
+        } else {
+            None
+        }
+    }).collect()
+}
+
+fn get_path(came_from: HashMap<(usize, usize), (usize, usize)>, node: (usize, usize)) -> Vec<(usize, usize)> {
+    let mut path = vec!(node);
+    let mut current = node;
+    while let Some(next_node) = came_from.get(&current) {
+        current = *next_node;
+        path.push(current);
+    }
+
+    path
+}
+
+fn find_reachable_nodes(from: (usize, usize), max_distance: usize, designer_num: u64) -> usize {
+    let mut closed_set = Vec::new();
+    let mut open_set = BinaryHeap::new();
+    open_set.push(Node{location: from, distance: 0});
+
+    let mut came_from = HashMap::new();
+    let mut seen = HashMap::new();
+
+    while let Some(node) = open_set.pop() {
+        if node.distance <= max_distance {
+            closed_set.push(node.location);
+
+            for neighbour in get_neighbours(node.location, designer_num) {
+                if !closed_set.contains(&neighbour) {
+                    let dist = node.distance + 1;
+
+                    {
+                        let ref have_visited = seen.get(&neighbour);
+                        if have_visited.is_none() {
+                            open_set.push(Node{location: neighbour, distance: dist});
+                        } else if *have_visited.unwrap() <= dist {
+                            continue;
+                        }
+                    }
+
+                    came_from.insert(neighbour, node.location);
+                    seen.insert(neighbour, dist);
+                }
+            }
         }
     }
 
-    let mut my_neighbour_map: HashMap<&str, i32> = HashMap::new();
-    let mut g = vec![];
-    for guest in guests {
-    	my_neighbour_map.insert(guest, 0);
-        g.push(guest);
+    closed_set.len()
+}
+
+fn find_shortest_route(from: (usize, usize), to: (usize, usize), designer_num: u64) -> Vec<(usize,usize)> {
+    let mut closed_set = Vec::new();
+    let mut open_set = BinaryHeap::new();
+    open_set.push(Node{location: from, distance: 0});
+
+    let mut came_from = HashMap::new();
+    let mut seen = HashMap::new();
+
+    while let Some(node) = open_set.pop() {
+        if node.location == to {
+            return get_path(came_from, node.location);
+        }
+
+        closed_set.push(node.location);
+
+        for neighbour in get_neighbours(node.location, designer_num) {
+            if !closed_set.contains(&neighbour) {
+                let dist = node.distance + 1;
+
+                {
+                    let ref have_visited = seen.get(&neighbour);
+                    if have_visited.is_none() {
+                        open_set.push(Node{location: neighbour, distance: dist});
+                    } else if *have_visited.unwrap() <= dist {
+                        continue;
+                    }
+                }
+
+                came_from.insert(neighbour, node.location);
+                seen.insert(neighbour, dist);
+            }
+        }
     }
 
-    g.push("Jack");
-    happiness.insert("Jack", my_neighbour_map);
+    panic!("Path not found");
+}
 
-    (happiness, g)
+fn draw_room((max_x,max_y): (usize, usize), designer_num: u64, path: &Vec<(usize, usize)>) {
+    std::process::Command::new("clear").status().unwrap();
+    for y in 0..max_y {
+        for x in 0..max_x {
+            if path.contains(&(x,y)) {
+                print!("O");
+            } else {
+                print!("{:?}", get_location_type((x as u64, y as u64), designer_num));
+            }      
+        }
+        println!("");
+    }
+}
+
+fn main() {
+    let path = find_shortest_route((1,1), (31,39), 1364);
+    draw_room((50, 50), 1364, &path);
+    println!("Num steps => {:?}", path.len() - 1);
+    println!("Num nodes => {:?}", find_reachable_nodes((1,1), 50, 1364));
+}
+
+#[test]
+fn test() {
+    let path = find_shortest_route((1,1), (7,4), 10);
+    assert_eq!(11, path.len()-1);
 }
